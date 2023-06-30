206aff362c656c74fe7dab950632e616fd265939
orioldedios <orioldedios@gmail.com>
2023-06-30 12:09:15 +0200

    scoring script done

---
+++ Catocallipsis/Assets/Scripts/Scoring.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class Scoring : MonoBehaviour
+{
+    public int score = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
